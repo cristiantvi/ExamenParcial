6c1c126bd939eaf68ec5a04fa5180d1e01fb5627
cristian torrico <cristian.torrico98@gmail.com>
2022-11-08 21:33:33 -0400

    se aplicarion los cambios

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Asistencia Profesional Web</title>
+    <title>Asistencia Profesional Web modificacion</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
